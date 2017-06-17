68947582cc1250f2d68b6954d7f6f0fb15318d71
OlyaKorabliova <korablyova@ucu.edu.ua>
2017-06-17 14:18:10 +0300

    Update callable_example.cpp

--- prARCH/callable_example.cpp
+++ prARCH/callable_example.cpp
@@ -1,56 +1,100 @@
 #include <iostream>
 #include <map>
 #include <vector>
-
-//#include <mutex>
+#include <math.h>
+#include <assert.h>
+#include <thread>
+#include <mutex>
+#include <deque>
+#include <condition_variable>
 
 
 using namespace std;
+mutex myMutex;
+mutex mx;
+condition_variable cv;
 
-map<string, int> counting_words_worker(const vector<string>& words){
-    map<string, int> localm;
-    for (size_t i = 0; i != words.size(); ++i) {
-            ++localm[words[i]];
+deque <map<string, int>> dm;
+int num = 0;
+
+void printMap(const map<string, int> &m) {
+    for (auto &elem : m) {
+        cout << elem.first << " " << elem.second << "\n";
     }
-    return localm;
 }
 
-map<string, int> reduce_f(map<string, int> m, const map<string, int>& localm) {
+
+map<string, int> reduce_f( map<string, int> m, const map<string, int>& localm ) {
+
     for (auto it = localm.begin(); it != localm.end(); ++it)
         m[it->first] += it->second;
-
     return m;
 }
 
-template <class V, class MF, class RF>
-auto func_tmpl(const V& v, MF fn1, RF fn2) ->
-            decltype( fn2(fn1(v[0]), fn1(v[0])) ) {
-    // V - масив блоків (векторів)
-    decltype(func_tmpl(v, fn1, fn2)) res;        // big map<string, int>
-    for (size_t i = 0; i < v.size(); ++i) {
-        auto x = fn1(v[i]);        //return small   map<string, int>
-        res = fn2(move(res), x);
+deque<map<string, int>> reducer(int num_of_threads){
+    unique_lock<mutex> uniqueLock(myMutex);
+    while (dm.size() > 1) {
+        map<string, int> map1 = dm.front();
+        dm.pop_front();
+        map<string, int> map2 = dm.front();
+        dm.pop_front();
+        uniqueLock.unlock();
+        map<string, int> map3 = reduce_f(map1, map2);
+
+        uniqueLock.lock();
+        dm.push_back(map3);
+    }if (dm.size() == 1 && num == num_of_threads) {
+        printMap(dm.front());
+    } else {
+        cv.wait(uniqueLock);
     }
-    for (auto i : res){
-        cout << i.first << " - " << i.second << endl;
+    return dm;
+}
+
+map<string, int> counting_words_worker( vector<string>::const_iterator beg, vector<string>::const_iterator  fin ){
+    map<string, int> localm;
+    for (auto i = beg; i != fin; ++i) {
+        ++localm[*i];
     }
+    {
+        lock_guard<mutex> lg(myMutex);
+        dm.push_back(localm);
+    }
+    cv.notify_one();
+    cv.notify_all();
+    num += 1;
+    return localm;
+}
+
+
+
+template <class MF, class RF, class I, class N>
+auto func_tmpl(I beg, I fin, MF fn1, RF fn2, N num_of_threads) -> decltype( fn2(num_of_threads) ) {
+    decltype(func_tmpl(beg, fin, fn1, fn2, num_of_threads)) res;        // big map<string, int>
+
+    size_t delta = (fin - beg) / num_of_threads;
+    vector<I> segments;
+    for (auto i = beg; i < fin - delta; i += delta) {
+        segments.push_back(i);
+    }
+    segments.push_back(fin);
+
+    thread myThreads [num_of_threads];
+    for (auto i = segments.begin(); i < segments.end()-1; ++i) {
+        myThreads[i - segments.begin()] = thread(fn1, *i, *(i+1));
+        fn2(num_of_threads);
+    }
+
+    for (auto& th : myThreads) th.join();
+    res = fn2(num_of_threads);
     return res;
 }
 
-void callE(const vector<vector<string>> &words){
-    func_tmpl(words, counting_words_worker, reduce_f);
-}
 
 int main()
 {
-    vector<string> v1 = {"asdf", "wert", "aaa", "qwerty"};
-    vector<string> v2 = {"asdf", "poiuy", "qwewrtdxfb", "qwerty"};
-    vector<vector<string>> v3;
-    v3.push_back(v1);
-    v3.push_back(v2);
-    callE(v3);
-
+    vector<string> v = {"aaaa", "bbbbb", "ccccc", "ddddd", "aaaa", "eeeee", "wwwwwwwww", "ccccc", "ccccc", "ccccc"};
+    cout<<"Words counter" << endl;
+    int num_of_threads = 3;
+    func_tmpl(v.begin(), v.end(), counting_words_worker, reducer, num_of_threads);
 }
-
-// template <class T>  T reduce_f(T m, const T& localm)
-// func_tmpl(words, counting_words_worker, reduce_f<map<string, int>>);

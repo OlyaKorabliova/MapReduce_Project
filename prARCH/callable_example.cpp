38eb32fa686f6e34fab06699d869b050faa3c81b
OlyaKorabliova <korablyova@ucu.edu.ua>
2017-06-17 20:57:34 +0300

    Update callable_example.cpp

--- prARCH/callable_example.cpp
+++ prARCH/callable_example.cpp
@@ -11,11 +11,174 @@
 
 using namespace std;
 mutex myMutex;
-mutex mx;
 condition_variable cv;
 
-deque <map<string, int>> dm;
 int num = 0;
+
+
+double func_calculation(int m, double x1, double x2) {
+    double sum1 = 0;
+    double sum2 = 0;
+    double g;
+    for (int i = 1; i <= m; ++i)
+    {
+        sum1 += i * cos((i + 1) * x1 + 1);
+        sum2 += i * cos((i + 1) * x2 + 1);
+    }
+
+    g = - sum1 * sum2;
+    return g;
+}
+
+using point_t = pair<double, double>;
+
+
+class Iter2Ddouble: public iterator<random_access_iterator_tag, point_t >
+{
+    double x0, x1, y0, y1, m, pr;
+    double x, y;
+public:
+    double get_pr() const {return pr; }
+    Iter2Ddouble(const vector<double>& data)
+    {
+        x0 = data[0];
+        x1  = data[1];
+        y0 = data[2];
+        y1  = data[3];
+        m = data[4];
+        pr = data[5];
+
+        x = x0;
+        y = y0;
+    }
+
+    Iter2Ddouble end() const{
+        Iter2Ddouble t(*this);
+        t.x = x1;
+        t.y = y1;
+        return t;
+    }
+
+    point_t operator*() const
+    {
+        return make_pair(x + pr / 2.0, y + pr / 2.0);
+    }
+
+    Iter2Ddouble& operator++()
+    {
+        x += pr;
+        if(x>x1)
+        {
+            x = x0;
+            y += pr;
+        }
+        return *this;
+    }
+
+    Iter2Ddouble& operator--()
+    {
+        x -= pr;
+        if(x<x0)
+        {
+            x = x1;
+            y -= pr;
+        }
+        return *this;
+    }
+
+    Iter2Ddouble& operator+=(size_t n)
+    {
+        //! TODO: write without loop!
+        for(size_t i = 0; i<n; ++i)
+            ++(*this);
+        return *this;
+    }
+    Iter2Ddouble& operator-=(size_t n)
+    {
+        //! TODO: write without loop!
+        for(size_t i = 0; i<n; ++i)
+            --(*this);
+        return *this;
+    }
+    ptrdiff_t operator-=(Iter2Ddouble itr)
+    {
+        //! TODO: write without loop!
+        ptrdiff_t diff = 0;
+        if( *this < itr) {
+            while (*this < itr) {
+                --itr;
+                --diff;
+            }
+        }else{
+            while ( !(*this < itr) ) {
+                ++itr;
+                ++diff;
+            }
+
+        }
+
+        return diff;
+    }
+
+    friend bool operator<(const Iter2Ddouble& itr1, const Iter2Ddouble& itr2);
+    friend bool operator==(const Iter2Ddouble& itr1, const Iter2Ddouble& itr2);
+};
+
+Iter2Ddouble operator+(Iter2Ddouble itr, size_t n)
+{
+    return itr += n;
+}
+Iter2Ddouble operator-(Iter2Ddouble itr, size_t n)
+{
+    return itr -= n;
+}
+
+ptrdiff_t  operator-(Iter2Ddouble itr_l, const Iter2Ddouble& itr_r)
+{
+    return itr_l -= itr_r;
+}
+
+bool operator<(const Iter2Ddouble& itr1, const Iter2Ddouble& itr2)
+{
+    if( itr1.y == itr2.y ) //! Bad code -- use abs|x - y | < eps
+    {
+        return itr1.x < itr2.x;
+    }
+    return itr1.y < itr2.y;
+}
+
+bool operator==(const Iter2Ddouble& itr1, const Iter2Ddouble& itr2)
+{
+    return itr1.y == itr2.y && itr1.x == itr2.x;  //! Bad code -- use abs|x - y | < eps
+}
+struct func_wrapper
+{
+    int m;
+    deque<double> d;
+    func_wrapper(int m_, deque<double> d_): m(m_), d(d_){};
+//    double operator()(const point_t& p)
+//    {
+//        return func_calculation(m, p.first, p.second);
+//    }
+    double operator()(const Iter2Ddouble& itr1, const Iter2Ddouble& itr2,  deque<double>& d)
+    {
+        double r = 0;
+        for(auto i = itr1; i <itr2; ++i) {
+            r += func_calculation(m, (*i).first, (*i).second) * itr1.get_pr() * itr1.get_pr();
+            // cout << (*i).first << " " <<  (*i).second << " " << r << endl;
+        }
+        d.push_back(r);
+        return r;
+    }
+
+};
+
+
+double thread_integration(double r, const double &sum) {
+    r += sum;
+    return r;
+}
+
 
 void printMap(const map<string, int> &m) {
     for (auto &elem : m) {
@@ -31,27 +194,30 @@ map<string, int> reduce_f( map<string, int> m, const map<string, int>& localm ) 
     return m;
 }
 
-deque<map<string, int>> reducer(int num_of_threads){
+
+template<class RF, class Dd>
+Dd reducer_univ(int num_of_threads, deque<Dd> &dm, RF (*fn2)(Dd, const Dd&)) {
     unique_lock<mutex> uniqueLock(myMutex);
     while (dm.size() > 1) {
-        map<string, int> map1 = dm.front();
+        Dd d1 = dm.front();
         dm.pop_front();
-        map<string, int> map2 = dm.front();
+        Dd d2 = dm.front();
         dm.pop_front();
         uniqueLock.unlock();
-        map<string, int> map3 = reduce_f(map1, map2);
+        Dd d3 = (*fn2)(d1, d2);
 
         uniqueLock.lock();
-        dm.push_back(map3);
-    }if (dm.size() == 1 && num == num_of_threads) {
-        printMap(dm.front());
-    } else {
+        dm.push_back(d3);
+        cout << "1" << endl;
+    }
+    if (dm.size() != 1 && num != num_of_threads){
+        cout << "3" << endl;
         cv.wait(uniqueLock);
     }
-    return dm;
+    return dm.front();
 }
 
-map<string, int> counting_words_worker( vector<string>::const_iterator beg, vector<string>::const_iterator  fin ){
+map<string, int> counting_words_worker( vector<string>::const_iterator beg, vector<string>::const_iterator  fin, deque<map<string, int>> &dm ){
     map<string, int> localm;
     for (auto i = beg; i != fin; ++i) {
         ++localm[*i];
@@ -68,10 +234,10 @@ map<string, int> counting_words_worker( vector<string>::const_iterator beg, vect
 
 
 
-template <class MF, class RF, class I, class N>
-auto func_tmpl(I beg, I fin, MF fn1, RF fn2, N num_of_threads) -> decltype( fn2(num_of_threads) ) {
-    decltype(func_tmpl(beg, fin, fn1, fn2, num_of_threads)) res;        // big map<string, int>
+template <class MF, class RF, class I, class N, class Dd>
+Dd func_tmpl(I beg, I fin, MF fn1, deque<Dd> &d, RF fn2, N num_of_threads) {
 
+    Dd res;
     size_t delta = (fin - beg) / num_of_threads;
     vector<I> segments;
     for (auto i = beg; i < fin - delta; i += delta) {
@@ -81,20 +247,29 @@ auto func_tmpl(I beg, I fin, MF fn1, RF fn2, N num_of_threads) -> decltype( fn2(
 
     thread myThreads [num_of_threads];
     for (auto i = segments.begin(); i < segments.end()-1; ++i) {
-        myThreads[i - segments.begin()] = thread(fn1, *i, *(i+1));
-        fn2(num_of_threads);
+        myThreads[i - segments.begin()] = thread(fn1, *i, *(i+1), ref(d));
+        reducer_univ(num_of_threads, d, fn2);
     }
 
     for (auto& th : myThreads) th.join();
-    res = fn2(num_of_threads);
+    res = reducer_univ(num_of_threads, d, fn2);
     return res;
 }
 
 
 int main()
 {
-    vector<string> v = {"aaaa", "bbbbb", "ccccc", "ddddd", "aaaa", "eeeee", "wwwwwwwww", "ccccc", "ccccc", "ccccc"};
-    cout<<"Words counter" << endl;
-    int num_of_threads = 3;
-    func_tmpl(v.begin(), v.end(), counting_words_worker, reducer, num_of_threads);
+//    deque <map<string, int>> dm;
+//    vector<string> v = {"aaaa", "bbbbb", "ccccc", "ddddd", "aaaa", "eeeee", "wwwwwwwww", "ccccc", "ccccc", "ccccc"};
+//    cout<< "Words counter" << endl;
+//    int num_of_threads = 3;
+//    map<string, int> m = func_tmpl(v.begin(), v.end(), counting_words_worker, dm, reduce_f, num_of_threads);
+//    printMap(m);
+
+    vector<double> data = {0, 1, 0, 1, 5, 0.001};
+    deque<double> d;
+    Iter2Ddouble itr(data);
+    cout << "Integral" << endl;
+    cout << func_tmpl(itr, itr.end(), func_wrapper(5, d), d, thread_integration, 4) << endl;
+
 }
